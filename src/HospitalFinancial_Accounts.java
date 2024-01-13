ed42224f60241d4e0cb5cbb135ddbfb152e53002
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 22:19:51 +0200

    Modify Hospital financial accounts class Add attributes Add public instance methods Add 3 constructors Add overriden method

--- src/HospitalFinancial_Accounts.java
+++ src/HospitalFinancial_Accounts.java
@@ -1,4 +1,40 @@
 
 public class HospitalFinancial_Accounts {
-
+   private double totalIncome;
+   private double totalExpenses;
+   public double balance;
+   
+   //constructors
+   public HospitalFinancial_Accounts() {
+	   
+   }
+   public HospitalFinancial_Accounts(double totalIncome, double totalExpenses,double balance) {
+	   
+   }
+   public HospitalFinancial_Accounts(double balance) {
+	   
+   }
+   
+   //public instance methods
+   public double getTotalIncome() {
+	   
+   }
+   public double getTotalExpenses() {
+	   
+   }
+   public double getBalance() {
+	   
+   }
+   public void addIncome(double income) {
+	   
+   }
+   public void addExpenses(double expenses) {
+	   
+   }
+   
+   @Override
+   public String toString() {
+	   
+   }
+   
 }
