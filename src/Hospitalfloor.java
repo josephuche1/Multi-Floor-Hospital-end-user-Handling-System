d50073e0ed0796f01a80da60418419cb338f8503
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 22:29:49 +0200

    Modify Hospital floors class add attributes Add 3 constuctors Add 3 instance methods and 1 overriden method

--- src/Hospitalfloor.java
+++ src/Hospitalfloor.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
 
-public class Hospitalfloor {
-
+public class HospitalFloor {
+   private String floorNumber;
+   private ArrayList<HospitalRoom> rooms;
+   
+   //constructors
+   public HospitalFloor() {
+	   
+   }
+   public HospitalFloor(String floorNumber) {
+	   
+   }
+   public HospitalFloor(String floorNumber, ArrayList<HospitalRoom> rooms) {
+	   
+   }
+   
+   //public instance methods
+   public String getFloorNumber() {
+	   
+   }
+   public ArrayList<HospitalRooms> getRooms(){
+	   
+   }
+   public void addRoom(HospitalRoom room) {
+	   
+   }
+   
+   @Override
+   public String toString() {
+	   
+   }
 }
