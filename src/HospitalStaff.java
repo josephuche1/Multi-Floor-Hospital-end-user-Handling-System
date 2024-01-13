b55e2e56f95956ff21e49421c6d6e8a71f695102
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-14 01:21:43 +0200

    Implemented methods in HospitalStaff class

--- src/HospitalStaff.java
+++ src/HospitalStaff.java
@@ -6,28 +6,35 @@ public abstract  class HospitalStaff {
   
   //constructors
   public HospitalStaff() {
+	  this.id = "";
+	  this.name = "";
+	  this.position = "";
 	  
   }
   public HospitalStaff(String id, String name) {
-	  
+	  this.id = id;
+	  this.name =name;
+	  this.position = "";
   }
   public HospitalStaff(String id, String name, String position) {
-	  
+	  this.id = id;
+	  this.name =name;
+	  this.position = position;	  
   }
   
   // public instance methods
   public String getId() {
-	  
+	  return this.id;
   }
   public String getName() {
-	  
+	  return this.name;
   }
   public String getPosition() {
-	  
+	  return this.position;
   }
   
   @Override
   public String toString() {
-	  
+	  return "ID: " + this.id + "\n Name: "+this.name+"\n Position: "+this.position;
   }
 }
