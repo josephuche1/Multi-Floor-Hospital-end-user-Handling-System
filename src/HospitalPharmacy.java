1508aecae2fbadd3d038807bebd2034cec107a11
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 22:09:38 +0200

    Modfy Hospital pharmacy class. Create Medicine class Add attribute Add 3 constructors > Add 4 instance methods and 1 overridden method

--- src/HospitalPharmacy.java
+++ src/HospitalPharmacy.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
 
 public class HospitalPharmacy {
-
+	private ArrayList<Medicine> medicines;
+	private static  int medicineCount;
+	
+	//constructors
+	public HospitalPharmacy() {
+		
+	}
+	public HospitalPharmacy(ArrayList<Medicine> medicines) {
+		
+	}
+	
+	public HospitalPharmacy(ArrayList<Medicine> medicines, int count) {
+		
+	}
+	
+	// public instance methods
+	public ArrayList<Medicine> getMedicines(){
+		
+	}
+	
+	public void addMedicine(Medicine medicine) {
+		
+	}
+	
+	public void removeMedicine(Medicine medicine) {
+		
+	}
+	public Medicine findMedicine(String name) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
 }
--- src/HospitalRoom.java
+++ src/HospitalRoom.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
 public class HospitalRoom {
-
+	private String roomNumber;
+	private ArrayList<HospitalEquipment> equipments;
+	
+	// Constructors
+	public  HospitalRoom() {
+		
+	}
+	public HospitalRoom(String roomNumber) {
+	     
+	}
+	public HospitalRoom(String roomNumber, ArrayList<HospitalEquipment> equipments) {
+		
+	}
+	
+	//public instance methods
+	public String getRoomNumber() {
+		
+	}
+	public ArrayList<Hospitalequipment> getEquipments(){
+		
+	}
+	public void addEquipment(HospitalEquipment equipment) {
+		
+	}
+	
+    @Override
+    public String toString();
 }
