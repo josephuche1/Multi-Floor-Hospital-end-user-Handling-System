2510ef8737fae686502f9e979e0a64904394b0cc
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-14 01:29:12 +0200

    Implemented methods in HospitalEquipment class

--- src/HospitalEquipment.java
+++ src/HospitalEquipment.java
@@ -6,27 +6,33 @@ public abstract class HospitalEquipment {
 	
 	//constructors
 	public HospitalEquipment() {
-		
+		this.id = "";
+		this.name="";
+		this.location="";
 	}
 	public HospitalEquipment(String id, String name) {
-		
+		this.id = id;
+		this.name = name;
+		this.location  = "";
 	}
 	public HospitalEquipment(String id, String name, String location) {
-		
+		this.id = id;
+		this.name =name;
+		this.location  = location;
 	}
 	
 	//public instance variables
 	public String getId() {
-		
+		return this.id;
 	}
 	public String getName() {
-		
+		return this.name;
 	}
 	public String getLocation() {
-		
+		return this.location;
 	}
 	public void setLocation(String Location) {
-		
+		this.location = location;
 	}
 	
 	@Override
