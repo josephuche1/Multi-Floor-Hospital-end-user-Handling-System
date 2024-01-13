fff72e7c32924054e55cd3095ece743791b1fda8
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 20:58:38 +0200

    Modify Patient class Add attributes Add 3 constructors Add 5 instance methods and 1 overriden method

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
 
 public class Patient {
-
+  private String id;
+  private String name;
+  private ArrayList<String> illness;
+  
+  //constructors
+  public Patient() {
+	  
+  }
+  public Patient(String id, String name) {
+	  
+  }
+  public Patient(String id, String name, String illness) {
+	  
+  }
+  
+  // Public instance methods
+  public String getId() {
+	  
+  }
+  public String getName() {
+	  
+  }
+  public ArrayList<String> getIllnesses() {
+	  
+  }
+  public void addIllness(String illness) {
+	  
+  }
+  public void setName(String name) {
+	  
+  }
+  
+  @Override
+  public String toString() {
+	  
+  }
+  
 }
