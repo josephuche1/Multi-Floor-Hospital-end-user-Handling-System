f99d0a9d67487ab8de966cf882e5ae3b74d86e95
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 21:06:27 +0200

    Modify Hospital staff class Add 3 constructors Add 4 instance methods Add 1 overridden method

--- src/HospitalStaff.java
+++ src/HospitalStaff.java
@@ -1,4 +1,36 @@
 
 public class HospitalStaff {
-
+  private String id;
+  private String name;
+  private String position;
+  
+  //constructors
+  public HospitalStaff() {
+	  
+  }
+  public HospitalStaff(String id, String name) {
+	  
+  }
+  public HospitalStaff(String id, String name, String position) {
+	  
+  }
+  
+  // public instance methods
+  public String getId() {
+	  
+  }
+  public String getName() {
+	  
+  }
+  public String getPosition() {
+	  
+  }
+  public void setPosition(String Position) {
+	  
+  }
+  
+  @Override
+  public String toString() {
+	  
+  }
 }
