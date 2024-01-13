2d5ccf273f30b04ca2e045a2ff1daf6169b7d5ed
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 19:43:23 +0200

    Modify Hospital class Add attrbutes Add 3 constructors Add 5 public instance method and one overriden method

--- src/Hospital.java
+++ src/Hospital.java
@@ -4,4 +4,44 @@ public class Hospital {
   private String name;
   private ArrayList<Patient> patients;
   private ArrayList<HospitalStaff> staff;
+  private ArrayList<HospitalRoom> rooms;
+  
+  //Constructors
+  public Hospital() {
+	  
+  }
+  
+  public Hospital(String name) {
+	  
+  }
+  
+  public Hospital(String name, ArrayList<Patient> patients, ArrayList<HospitalStaff> staff, ArrayList<HospitalRoom> rooms) {
+	  
+  }
+  
+  // public instance methods
+  public String getName() {
+	  
+  }
+  
+  public void setName() {
+	  
+  }
+  
+  public void addPatient(Patient patient) {
+	  
+  }
+  
+  public void addStaff(HospitalStaff staff) {
+	  
+  }
+  
+  public void addRoom(HospitalRoom room) {
+	  
+  }
+  
+  @Override
+  public String toString() {
+	  
+  }
 }
