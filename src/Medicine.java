8a8a9c98d17c551c4fa3354d495bed3a97e9a660
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-13 22:43:41 +0200

    modify medicine class Add Attribute Add instance methosd Add constructor Override 1 method

--- src/Medicine.java
+++ src/Medicine.java
@@ -1,4 +1,31 @@
 
 public class Medicine {
-
+	private String id;
+	private String name;
+	private String description;
+	private double price;
+	
+	//constructor
+	public Medicine(String id, String name, String description, double price) {
+		
+	}
+	
+	// public instance methods
+	public String getId() {
+		
+	}
+	public String getName() {
+		
+	}
+	public String getDesription() {
+		
+	}
+	public double getPrice() {
+		
+	}
+	
+    @Override
+    public String toString() {
+    	
+    }
 }
