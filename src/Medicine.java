323ac64b0af93b80890e4dede162ed2eabe88dc3
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-14 02:05:13 +0200

    Implemented methods in Medicine class

--- src/Medicine.java
+++ src/Medicine.java
@@ -1,31 +1,33 @@
-
 public class Medicine {
-	private String id;
-	private String name;
-	private String description;
-	private double price;
-	
-	//constructor
-	public Medicine(String id, String name, String description, double price) {
-		
-	}
-	
-	// public instance methods
-	public String getId() {
-		
-	}
-	public String getName() {
-		
-	}
-	public String getDesription() {
-		
-	}
-	public double getPrice() {
-		
-	}
-	
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    
+    //constructor
+    public Medicine(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    
+    // public instance methods
+    public String getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    
     @Override
     public String toString() {
-    	
+        return "Medicine ID: " + this.id + ", Name: " + this.name + ", Description: " + this.description + ", Price: " + this.price;
     }
 }
