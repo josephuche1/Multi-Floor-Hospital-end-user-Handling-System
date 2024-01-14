74987575e8cc2c54c6a3a9253801b0deec796014
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-14 02:03:01 +0200

    Implemnt methods in hospital floor class

--- src/Hospitalfloor.java
+++ src/Hospitalfloor.java
@@ -6,28 +6,36 @@ public class HospitalFloor {
    
    //constructors
    public HospitalFloor() {
+	   this.floorNumber = "";
+	   this.rooms = new ArrayList<HospitalRoom>();
 	   
    }
    public HospitalFloor(String floorNumber) {
-	   
+	   this.floorNumber = floorNumber;
+	   this.rooms = new ArrayList<HospitalRoom>();
    }
    public HospitalFloor(String floorNumber, ArrayList<HospitalRoom> rooms) {
+	   this.floorNumber = floorNumber;
+	   this.rooms = new ArrayList<HospitalRoom>();
 	   
+	   for(HospitalRoom room : rooms) {
+		   this.rooms.add(room);
+	   }
    }
    
    //public instance methods
    public String getFloorNumber() {
-	   
+	   return this.floorNumber;
    }
-   public ArrayList<HospitalRooms> getRooms(){
-	   
+   public ArrayList<HospitalRoom> getRooms(){
+	   return this.rooms;
    }
    public void addRoom(HospitalRoom room) {
-	   
+	   this.rooms.add(room);
    }
    
    @Override
    public String toString() {
-	   
+	   return "Floor Number: "+ this.floorNumber + ", Number of rooms: " +this.rooms.size();
    }
 }
