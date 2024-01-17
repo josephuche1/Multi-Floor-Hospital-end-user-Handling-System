fa951d7147fd4e4b01d0c6b7c3e7b0737f378fbf
PARSIVALJ <uchejesusboy@gmail.com>
2024-01-17 08:41:09 +0200

    Document hospital paharmac class

--- src/HospitalPharmacy.java
+++ src/HospitalPharmacy.java
@@ -1,15 +1,33 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * HospitalPharmacy is a class that represents the pharmacy within a hospital.
+ * It manages an inventory of medicines and tracks the count of different medicines available.
+ * This class is responsible for adding, removing, and finding medicines in the pharmacy's inventory.
+ * It implements Serializable to allow pharmacy data to be saved and restored as needed.
+ * 
+ * @author Uche Joseph
+ * @version 1.0
+ */
 public class HospitalPharmacy implements Serializable{
 	private ArrayList<Medicine> medicines;
 	private int countMedicine;
 	
-	//constructors
+
+    /**
+     * Default constructor that initializes a new HospitalPharmacy with an empty list of medicines.
+     */
 	public HospitalPharmacy() {
 		this.medicines = new ArrayList<Medicine>();
 		countMedicine = 0;
 	}
+	
+    /**
+     * Constructor that initializes a new HospitalPharmacy with a provided list of medicines.
+     *
+     * @param medicines The initial list of medicines to be added to the pharmacy.
+     */
 	public HospitalPharmacy(ArrayList<Medicine> medicines) {
 		this.medicines = new ArrayList<Medicine>();
 		for(Medicine medicine : medicines) {
@@ -18,21 +36,41 @@ public class HospitalPharmacy implements Serializable{
 		countMedicine = medicines.size();
 	}
 	
-	// public instance methods
+    /**
+     * Retrieves the list of medicines in the pharmacy.
+     *
+     * @return An ArrayList of Medicine objects.
+     */
 	public ArrayList<Medicine> getMedicines(){
 		return this.medicines;
 	}
 	
+    /**
+     * Adds a medicine to the pharmacy's inventory and increments the medicine count.
+     *
+     * @param medicine The Medicine object to be added.
+     */
 	public void addMedicine(Medicine medicine) {
 		this.medicines.add(medicine);
 		countMedicine++;
 	}
 	
+    /**
+     * Removes a medicine from the pharmacy's inventory and decrements the medicine count.
+     *
+     * @param medicine The Medicine object to be removed.
+     */
 	public void removeMedicine(Medicine medicine) {
 		this.medicines.remove(medicine);
 		countMedicine--;
 	}
 	
+    /**
+     * Finds a medicine by its name in the pharmacy's inventory.
+     *
+     * @param name The name of the medicine to find.
+     * @return The Medicine object if found, or null otherwise.
+     */
 	public Medicine findMedicine(String name) {
 		for(Medicine medicine : this.medicines) {
 			if(medicine.getName() == name) {
@@ -45,6 +83,12 @@ public class HospitalPharmacy implements Serializable{
 		return this.countMedicine;
 	}
 	
+	
+    /**
+     * Retrieves the count of medicines in the pharmacy.
+     *
+     * @return The count of medicines as an integer.
+     */
 	@Override
 	public String toString() {
 		return "Pharmacy: \n Number of medicine: "+this.medicines.size();
--- src/Hospitalfloor.java
+++ src/Hospitalfloor.java
@@ -1,22 +1,46 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * HospitalFloor is a class that represents a floor within a hospital.
+ * It contains a list of rooms on the floor and tracks the number of rooms.
+ * This class is responsible for managing the rooms and their count on a particular floor.
+ * It implements Serializable to allow floor data to be saved and restored as needed.
+ * 
+ * @author Uche Joseph
+ * @version 1.0
+ */
 public class HospitalFloor implements Serializable{
    private String floorNumber;
    private ArrayList<HospitalRoom> rooms;
    private int roomCount;
    
-   //constructors
+   /**
+    * Default constructor that initializes a new HospitalFloor with an empty floor number and room list.
+    */
    public HospitalFloor() {
 	   this.floorNumber = "";
 	   this.rooms = new ArrayList<HospitalRoom>();
 	   roomCount =0;
    }
+   
+   /**
+    * Constructor that initializes a new HospitalFloor with the specified floor number and an empty room list.
+    *
+    * @param floorNumber The floor number to be assigned to the floor.
+    */
    public HospitalFloor(String floorNumber) {
 	   this.floorNumber = floorNumber;
 	   this.rooms = new ArrayList<HospitalRoom>();
 	   roomCount =0;
    }
+   
+   /**
+    * Constructor that initializes a new HospitalFloor with the specified floor number and room list.
+    *
+    * @param floorNumber The floor number to be assigned to the floor.
+    * @param rooms       The initial list of rooms on the floor.
+    */
    public HospitalFloor(String floorNumber, ArrayList<HospitalRoom> rooms) {
 	   this.floorNumber = floorNumber;
 	   this.rooms = new ArrayList<HospitalRoom>();
@@ -27,22 +51,48 @@ public class HospitalFloor implements Serializable{
 	   roomCount = rooms.size();
    }
    
-   //public instance methods
+   /**
+    * Retrieves the floor number of this HospitalFloor.
+    *
+    * @return The floor number as a string.
+    */
    public String getFloorNumber() {
 	   return this.floorNumber;
    }
+   
+   /**
+    * Retrieves the list of rooms on this HospitalFloor.
+    *
+    * @return An ArrayList of HospitalRoom objects.
+    */
    public ArrayList<HospitalRoom> getRooms(){
 	   return this.rooms;
    }
+   
+   /**
+    * Adds a room to the list of rooms on this floor and increments the room count.
+    *
+    * @param room The HospitalRoom to add to the floor.
+    */
    public void addRoom(HospitalRoom room) {
 	   this.rooms.add(room);
 	   roomCount++;
    }
+   
+   /**
+    * Retrieves the count of rooms on this HospitalFloor.
+    *
+    * @return The number of rooms as an integer.
+    */
    public int getRoomCount() {
 	   return this.roomCount;
    }
    
-   
+   /**
+    * Provides a string representation of the HospitalFloor, including the floor number and the number of rooms.
+    *
+    * @return A string detailing the floor number and room count.
+    */
    @Override
    public String toString() {
 	   return "Floor Number: "+ this.floorNumber + ", Number of rooms: " +this.rooms.size();
